dc500a6233d5a7ae80fe2b09cd3c248656e4fadd
Aisamuddin <aisam0094@gmail.com>
2023-02-23 10:30:57 +0800

    add tailwind color theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,22 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'primary': '#C98986',
+      'primary-focus': '#8A565C',
+      'secondary': '#F6BDD1',
+      'secondary-focus': '#F3A5C0',
+      'accent': '#FFD60A',
+      'accent-focus': '#E0BB00',
+      'black': '#011627',
+      'gray': '#777777',
+      'gray-focus': '#4a4949',
+      'white': '#fdfffc',
+      'info': '#5bc0de',
+      'success': '#5cb85c',
+      'warning': '#f0ad4e',
+      'error': '#d9534f',
+    }
   },
   plugins: [],
 }
