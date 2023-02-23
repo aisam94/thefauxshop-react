6ea0f4d8617cb6c01723fa5a24c58da9a0987b52
Aisamuddin <aisam0094@gmail.com>
2023-02-23 16:51:31 +0800

    edit about page

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,8 +1,33 @@
 function AboutPage() {
   return (
-    <>
-      <div>About</div>
-    </>
+    <div className="about-width-container text-center">
+      <h1 className="text-center uppercase my-5">About</h1>
+      <div>
+        <p className="">
+          Welcome to our e-commerce website, where you can find a wide range of
+          products that cater to your needs. While we may be a fake e-commerce
+          website as the name implies, we are passionate about creating an
+          online shopping experience that is both seamless and enjoyable.
+        </p>
+        <br />
+        <p className="">
+          At our online store, you will find a vast selection of high-quality
+          products that are carefully curated to meet your specific needs. From
+          bags to wallets, we have a wide variety of items that are sure to suit
+          your tastes.
+        </p>
+        <br />
+        <p className="">
+          We take pride in providing exceptional customer service and fast
+          shipping, ensuring that your online shopping experience is stress-free
+          and convenient. Whether you are shopping for yourself or looking for
+          the perfect gift for a loved one, we are confident that you will find
+          exactly what you need at our e-commerce website. Thank you for
+          visiting our website, and we hope that you enjoy browsing our
+          selection of products.
+        </p>
+      </div>
+    </div>
   );
 }
 
