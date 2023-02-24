24c059755ca9e960e43cdb482336a8616cd63319
Aisamuddin <aisam0094@gmail.com>
2023-02-24 23:21:30 +0800

    add contact page

---
+++ src/routes/contact.jsx
@@ -0,0 +1,29 @@
+function Contact() {
+  return (
+    <>
+      <div className="about-width-container text-center">
+        <h1 className="text-center uppercase my-5">Contact</h1>
+        <div className="">
+          <span>Any questions about our products?</span>
+          <br />
+          <span>Or mayhaps you just wanna say hi?</span>
+          <br />
+          <h3 className="my-4">-</h3>
+          <br />
+          <span className="text-primary font-bold">write us at</span>
+          <p>thefauxshop@fakeemail.com</p>
+          <h3 className="my-4">-</h3>
+          <br />
+          <span className="text-primary font-bold">call us on</span>
+          <br />
+          <span>+010 666 9999</span>
+          <h3 className="my-4">-</h3>
+          <br />
+          <span>We love to hear more from you!</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Contact;
