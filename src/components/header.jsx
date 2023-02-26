e7c69e5e01ef9faef41e1e3600d7d8a292009fca
Aisamuddin <aisam0094@gmail.com>
2023-02-26 22:41:37 +0800

    add some icons

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,17 +6,25 @@ function Header() {
       <h1 className="text-3xl text-center my-2">TheFauxShop</h1>
       <nav className="flex flex-col bg-red-300">
         <ul className="flex w-1/2 mx-auto justify-around text-center list-none">
-          <li>
+          <li className="flex items-center space-x-1 hover:scale-125 ease-in-out transition">
             <Link to={"/"}>Home</Link>
+            <img className="header-icons w-5 h-5" src="/icons/home.svg" />
           </li>
-          <li>
+          <li className="flex items-center space-x-1 hover:scale-125 ease-in-out transition">
             <Link to={"/products"}>Shop</Link>
+            <img className="header-icons w-5 h-5" src="/icons/shop.svg" />
           </li>
-          <li>
+          <li className="flex items-center space-x-1 hover:scale-125 ease-in-out transition">
             <Link to={"/about"}>About</Link>
+            <img className="header-icons w-5 h-5" src="/icons/about.svg" />
           </li>
-          <li>
+          <li className="flex items-center space-x-1 hover:scale-125 ease-in-out transition">
             <Link to={"/contact"}>Contact</Link>
+            <img className="header-icons w-5 h-5" src="/icons/contact.svg" />
+          </li>
+          <li className="flex items-center space-x-1 hover:scale-125 ease-in-out transition">
+            <Link to={"/cart"}>Cart</Link>
+            <img className="header-icons w-5 h-5" src="/icons/cart.svg" />
           </li>
         </ul>
       </nav>
--- src/routes/product.jsx
+++ src/routes/product.jsx
@@ -19,11 +19,27 @@ export default function ProductPage() {
         />
         <div className="product-right-side flex flex-col px-5 pt-2">
           <span className="text-2xl font-thin text-center">{product.name}</span>
-          <span className="text-gray my-2 text-center">RM {product.price.toFixed(2)}</span>
-          <div className="my-3 p-3 capitalize bg-secondary-focus hover:bg-secondary text-white text-center cursor-pointer w-full">add</div>
+          <span className="text-gray my-2 text-center">
+            RM {product.price.toFixed(2)}
+          </span>
+          <div className="flex space-x-2 justify-center items-center my-3 p-3 capitalize bg-secondary hover:bg-secondary-focus text-white text-center cursor-pointer w-full">
+            <span>add</span>
+            <img
+              className="header-icons w-6 h-6"
+              src="/icons/add-to-cart.svg"
+            />
+          </div>
           <span className="text-primary my-2">description</span>
-          <span className="overflow-hidden text-ellipsis px-1 py-1">{product.description}</span>
-          <Link to={'/'} className="mt-auto p-3 capitalize bg-accent hover:bg-accent-focus text-white text-center cursor-pointer w-full justify-end">go back</Link>
+          <span className="overflow-hidden text-ellipsis px-1 py-1">
+            {product.description}
+          </span>
+          <Link
+            to={"/"}
+            className="flex space-x-2 mt-auto p-3 capitalize bg-accent hover:bg-accent-focus text-white text-center cursor-pointer w-full justify-center items-center"
+          >
+            <span>go back</span>
+            <img className="header-icons w-5 h-5" src="/icons/back.svg" />
+          </Link>
         </div>
       </div>
     </>
