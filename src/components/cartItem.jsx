b7f8c748555de043923418383815282d4620b9a4
Aisamuddin <aisam0094@gmail.com>
2023-02-28 21:49:45 +0800

    add buttons for cart items

--- src/components/cartItem.jsx
+++ src/components/cartItem.jsx
@@ -1,8 +1,25 @@
 import { Link } from "react-router-dom";
 import { useCartStore } from "../store/cartStore";
+import { useState } from "react";
 
 function CartItem({ product }) {
   const { removeItem } = useCartStore();
+  const [itemNum, setItemNum] = useState(1);
+
+  function handleItemNum(e) {
+    e.preventDefault();
+    if (e.target.value < 1) return;
+    setItemNum(e.target.value);
+  }
+
+  function decrementItemNum(e) {
+    e.preventDefault();
+    if (itemNum > 1) setItemNum(+itemNum - 1);
+  }
+
+  function incrementItemNum() {
+    setItemNum(+itemNum + 1);
+  }
 
   return (
     <div className="flex border-b-2 border-secondary mt-2 pb-2 mb-2">
@@ -12,6 +29,26 @@ function CartItem({ product }) {
       <div className="flex flex-col">
         <span>{product.name}</span>
         <span className="text-gray">RM {product.price.toFixed(2)}</span>
+        <div className="mt-2">
+          <button
+            className="border border-secondary w-6 text-white bg-secondary hover:bg-secondary-focus"
+            onClick={decrementItemNum}
+          >
+            -
+          </button>
+          <input
+            className="item-num-input border-secondary"
+            type="number"
+            value={itemNum}
+            onChange={handleItemNum}
+          ></input>
+          <button
+            className="border border-secondary w-6 text-white bg-secondary hover:bg-secondary-focus"
+            onClick={incrementItemNum}
+          >
+            +
+          </button>
+        </div>
       </div>
       <div className="ml-auto pr-1 flex flex-col items-center">
         <span>RM {product.price.toFixed(2)}</span>
