ac276b9febf098d8c6c7097dd1549b02e798a17b
Aisamuddin <aisam0094@gmail.com>
2023-02-28 17:11:54 +0800

    add basic calculation in cart

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -1,15 +1,51 @@
 import { useCartStore } from "../store/cartStore";
 import CartItem from "../components/cartItem";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function Cart() {
   const { cart, removeAllItems } = useCartStore();
+  const [subTotalCost, setSubTotalCost] = useState(0);
+  const [shippingCost, setShippingCost] = useState(10);
+  const [totalCost, setTotalCost] = useState(0);
+
+  const defaultShippingCost = 10;
+
+  function getSubTotalCost() {
+    let sum = 0;
+    for (let i = 0; i < cart.length; i++) {
+      sum += cart[i].price;
+    }
+    return sum;
+  }
+  let subTotal = getSubTotalCost();
+
+  function getShippingCost() {
+    if (cart.length === 0) return 0;
+    return defaultShippingCost;
+  }
+
+  function getTotalCost() {
+    return subTotal + getShippingCost();
+  }
+
+  useEffect(() => {
+    setSubTotalCost(subTotal);
+    setShippingCost(getShippingCost);
+    setTotalCost(getTotalCost);
+  }, [cart]);
 
   return (
     <div className="cart-container flex mt-5">
       <div className="cart-left-side w-3/4 pr-3 relative">
         <div>
           <h3 className="cart-header">My Cart</h3>
-          <span className="absolute top-0 right-0 pr-4 cursor-pointer hover:opacity-70" onClick={removeAllItems}>Remove all</span>
+          <span
+            className="absolute top-0 right-0 pr-4 cursor-pointer hover:opacity-70"
+            onClick={removeAllItems}
+          >
+            Remove all
+          </span>
         </div>
         {cart.length > 0 ? (
           cart.map((product) => {
@@ -29,16 +65,16 @@ function Cart() {
           <div className="border-b-2 border-secondary mt-2 pb-2">
             <div className="flex justify-between">
               <span>Subtotal</span>
-              <span>RM 100</span>
+              <span>RM {subTotalCost.toFixed(2)}</span>
             </div>
             <div className="flex justify-between">
               <span>Shipping</span>
-              <span>RM 10</span>
+              <span>RM {shippingCost.toFixed(2)}</span>
             </div>
           </div>
           <div className="flex justify-between items-center">
             <span className="text-xl font-bold">Total</span>
-            <span>RM 110</span>
+            <span>RM {totalCost.toFixed(2)}</span>
           </div>
         </div>
       </div>
