f6add69f766b32e67965dc12dd96a7a1f06502c4
Aisamuddin <aisam0094@gmail.com>
2023-03-01 20:40:57 +0800

    add persist to cart store

--- src/store/cartStore.js
+++ src/store/cartStore.js
@@ -1,8 +1,13 @@
 import { create } from "zustand";
+import { persist } from 'zustand/middleware'
 
-export const useCartStore = create((set) => ({
+export const useCartStore = create(persist((set) => ({
   cart: [],
   addItem: (newItem) => set((state) => ({ cart: [...state.cart, newItem] })),
   removeItem: (id) => set((state) => ({ cart: state.cart.filter(item => item.id !== id) })),
   removeAllItems: () => set({ cart: [] }),
-}));
+}),
+  {
+    name: "cart-storage",
+  }
+));
